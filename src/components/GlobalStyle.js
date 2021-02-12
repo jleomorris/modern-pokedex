e824a74aa864372b4606f60240b147e9787b325f
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-02-12 23:31:57 +0000

    Update global styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -7,8 +7,14 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Montserrat', sans-serif;
   }
+
+  // Conditional classes
+  .dark-mode {
+      background: rgba(0,0,0,0.9);
+  }
   
   // Media Queries
+
   html {
       @media (max-width: 1700px) {
           font-size: 75%;
