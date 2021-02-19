3b467d957254ab7383a4e2e74f264a1bd7ba87e3
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-02-19 21:47:26 +0000

    Add evolution chart to InnerDetails

--- src/components/InnerDetails.js
+++ src/components/InnerDetails.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // Styled components
 import styled from 'styled-components';
+// Axios
+import axios from 'axios';
+// Redux
+import { useSelector } from 'react-redux';
 // Images
 import forwardArrow from '../img/icons8-forward-arrow-50.png';
 import backArrow from '../img/icons8-reply-arrow-50.png';
@@ -14,6 +18,61 @@ const InnerDetails = ({
   ownMoves,
   learnableMoves,
 }) => {
+  // Redux
+  const pokemonData = useSelector((state) => state.pokemon.pokemonData);
+  const pokemonData2 = useSelector((state) => state.pokemon.pokemonData2);
+  // State
+  const [evolutionData, setEvolutionData] = useState();
+  const [baseStage, setBaseStage] = useState();
+  const [firstEvolution, setFirstEvolution] = useState();
+  const [secondEvolution, setSecondEvolution] = useState();
+
+  // Get evolution data
+  useEffect(() => {
+    // debugger;
+    axios
+      .get(selectedPokemon2[0].evolution_chain.url)
+      .then((response) => {
+        // handle success
+        console.log('response', response);
+        setEvolutionData(response.data.chain.evolves_to);
+
+        // Execution breaks here
+        const baseStageName = response.data.chain.species.name;
+        const firstEvolutionName =
+          response.data.chain.evolves_to[0].species.name;
+        const secondEvolutionName = response.data.chain.evolves_to[0]
+          .evolves_to[0]
+          ? response.data.chain.evolves_to[0].evolves_to[0].species.name
+          : [];
+        // debugger;
+
+        console.log('baseStageName', baseStageName);
+        console.log('firstEvolutionName', firstEvolutionName);
+        console.log('secondEvolutionName', secondEvolutionName);
+        console.info('pokemonData', pokemonData);
+
+        // debugger;
+
+        setBaseStage(
+          pokemonData.filter((pokemon) => pokemon.name === baseStageName)
+        );
+        setFirstEvolution(
+          pokemonData.filter((pokemon) => pokemon.name === firstEvolutionName)
+        );
+        setSecondEvolution(
+          pokemonData.filter((pokemon) => pokemon.name === secondEvolutionName)
+        );
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+      });
+  }, [selectedPokemon2]);
+
   return (
     <StyledInnerDetails className="inner-details">
       {/* <h2 className="feature-title">{selectedPokemon[0].name}</h2> */}
@@ -62,6 +121,97 @@ const InnerDetails = ({
                     selectedPokemon2[0].flavor_text_entries[0].flavor_text
                   )} */}
         </p>
+      </div>
+      <div className="evolution-chart-container">
+        <h3>Evolution chart</h3>
+        <div className="evolution-chart">
+          {baseStage && (
+            <div className="evolution-card">
+              <img
+                src={Object.values(baseStage[0].sprites.other)[1].front_default}
+                style={{ height: `${baseStage[0].height * 25}px` }}
+                alt="first evolution"
+              />
+              <div className="name-type-container">
+                <p className="base-stage-name">{baseStage[0].name}</p>
+                {baseStage[0].types.map((type) => (
+                  <p
+                    style={{
+                      background: convertTypeToColor(type.type.name),
+                    }}
+                    className="evolution-type"
+                  >
+                    {type.type.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+          )}
+          {firstEvolution && (
+            <div className="evolution-card">
+              <img
+                src={
+                  Object.values(firstEvolution[0].sprites.other)[1]
+                    .front_default
+                }
+                style={{ height: `${firstEvolution[0].height * 25}px` }}
+                alt="first evolution"
+              />
+              <div className="name-type-container">
+                <p className="first-evolution-name">{firstEvolution[0].name}</p>
+                {firstEvolution[0].types.map((type) => (
+                  <p
+                    style={{
+                      background: convertTypeToColor(type.type.name),
+                    }}
+                    className="evolution-type"
+                  >
+                    {type.type.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+          )}
+          {/* TypeError: Cannot read property 'sprites' of undefined */}
+          {typeof secondEvolution !== 'undefined' && (
+            <div className="evolution-card">
+              <img
+                src={
+                  typeof secondEvolution !== 'undefined'
+                    ? Object.values(secondEvolution[0].sprites.other)[1]
+                        .front_default
+                    : ''
+                }
+                style={{
+                  height: `${secondEvolution[0].height * 25}px`,
+                  marginBottom:
+                    secondEvolution[0].name === 'charizard' ||
+                    secondEvolution[0].name === 'venusaur' ||
+                    secondEvolution[0].name === 'poliwrath'
+                      ? '-4.5rem'
+                      : '',
+                  //   marginBottom: `${secondEvolution[0].name} === "charizard" || ${secondEvolution[0].name} === "venusaur" ? '4.5rem' : ''`,
+                }}
+                alt="second evolution"
+              />
+              <div className="name-type-container">
+                <p className="second-evolution-name">
+                  {secondEvolution[0].name}
+                </p>
+                {secondEvolution[0].types.map((type) => (
+                  <p
+                    style={{
+                      background: convertTypeToColor(type.type.name),
+                    }}
+                    className="evolution-type"
+                  >
+                    {type.type.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <div className="move-container">
         <h3>Own moves & level learned</h3>
@@ -189,6 +339,82 @@ const StyledInnerDetails = styled.div`
     width: 55%;
     margin: 1rem 0rem;
     text-align: right;
+  }
+
+  .evolution-chart-container {
+    width: 70%;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    h3 {
+      color: white;
+      width: 100%;
+      text-transform: uppercase;
+      margin: 1rem 0rem;
+      margin: 2rem 0rem;
+    }
+
+    .evolution-chart {
+      position: relative;
+      width: fit-content;
+      max-width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 2rem;
+      padding: 3rem;
+      margin: 2rem 0rem;
+      background: rgba(256, 256, 256, 0.7);
+
+      .evolution-card {
+        display: flex;
+        justify-content: flex-end;
+        flex-direction: column;
+        align-items: center;
+        height: 100%;
+        margin: 0rem 1rem;
+      }
+
+      img {
+        width: auto;
+      }
+
+      .name-type-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: end;
+        align-items: center;
+        height: 140px;
+        margin-top: 1rem;
+
+        .base-stage-name,
+        .first-evolution-name,
+        .second-evolution-name {
+          margin: 0.25rem 0rem;
+          border-radius: 2rem;
+          text-align: center;
+          width: 80%;
+          font-family: 'Bebas Neue', cursive;
+          text-transform: uppercase;
+          font-size: 1.5rem;
+          color: black;
+          letter-spacing: 0.25rem;
+        }
+
+        .evolution-type {
+          margin: 0.25rem 0rem;
+          border-radius: 2rem;
+          text-align: center;
+          width: 200px;
+          font-family: 'Bebas Neue', cursive;
+          text-transform: uppercase;
+          font-size: 1.25rem;
+          color: black;
+          box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+        }
+      }
+    }
   }
 
   .move-container {
