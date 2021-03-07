42c20b1047be73d13729c47a08017ed2fde40832
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-03-07 03:51:36 +0000

    Group location areas by game

--- src/components/LocationArea.js
+++ src/components/LocationArea.js
@@ -17,7 +17,110 @@ const LocationArea = ({ selectedPokemon }) => {
         // handle success
         console.log('response', response);
 
-        setLocationAreaEncounters(response.data);
+        const locationData = response.data;
+
+        const diamond = [];
+        const pearl = [];
+        const platinum = [];
+        const gold = [];
+        const silver = [];
+        const crystal = [];
+        const soulSilver = [];
+        const heartGold = [];
+        const red = [];
+        const blue = [];
+        const yellow = [];
+        const green = [];
+        const fireRed = [];
+        const leafGreen = [];
+        const x = [];
+        const y = [];
+        const ruby = [];
+        const sapphire = [];
+        const emerald = [];
+        const black = [];
+        const white = [];
+        const black2 = [];
+        const white2 = [];
+
+        locationData.forEach((location) => {
+          location.version_details.forEach((ver) => {
+            if (ver.version.name === 'diamond')
+              diamond.push(location.location_area.name);
+            if (ver.version.name === 'pearl')
+              pearl.push(location.location_area.name);
+            if (ver.version.name === 'platinum')
+              platinum.push(location.location_area.name);
+            if (ver.version.name === 'gold')
+              gold.push(location.location_area.name);
+            if (ver.version.name === 'silver')
+              silver.push(location.location_area.name);
+            if (ver.version.name === 'crystal')
+              crystal.push(location.location_area.name);
+            if (ver.version.name === 'soulsilver')
+              soulSilver.push(location.location_area.name);
+            if (ver.version.name === 'heartgold')
+              heartGold.push(location.location_area.name);
+            if (ver.version.name === 'red')
+              red.push(location.location_area.name);
+            if (ver.version.name === 'blue')
+              blue.push(location.location_area.name);
+            if (ver.version.name === 'yellow')
+              yellow.push(location.location_area.name);
+            if (ver.version.name === 'green')
+              green.push(location.location_area.name);
+            if (ver.version.name === 'firered')
+              fireRed.push(location.location_area.name);
+            if (ver.version.name === 'leafgreen')
+              leafGreen.push(location.location_area.name);
+            if (ver.version.name === 'x') x.push(location.location_area.name);
+            if (ver.version.name === 'y') y.push(location.location_area.name);
+            if (ver.version.name === 'ruby')
+              ruby.push(location.location_area.name);
+            if (ver.version.name === 'sapphire')
+              sapphire.push(location.location_area.name);
+            if (ver.version.name === 'emerald')
+              emerald.push(location.location_area.name);
+            if (ver.version.name === 'black-2')
+              black2.push(location.location_area.name);
+            if (ver.version.name === 'white-2')
+              white2.push(location.location_area.name);
+            if (ver.version.name === 'black')
+              black.push(location.location_area.name);
+            if (ver.version.name === 'white')
+              white.push(location.location_area.name);
+          });
+        });
+
+        const allVersionData = [
+          { name: 'Red', areas: red },
+          { name: 'Blue', areas: blue },
+          { name: 'Yellow', areas: yellow },
+          { name: 'Green', areas: green },
+          { name: 'Gold', areas: gold },
+          { name: 'Silver', areas: silver },
+          { name: 'Crystal', areas: crystal },
+          { name: 'Ruby', areas: ruby },
+          { name: 'Sapphire', areas: sapphire },
+          { name: 'Emerald', areas: emerald },
+          { name: 'Diamond', areas: diamond },
+          { name: 'Pearl', areas: pearl },
+          { name: 'Platinum', areas: platinum },
+          { name: 'Black', areas: black },
+          { name: 'White', areas: white },
+          { name: 'Black 2', areas: black2 },
+          { name: 'White 2', areas: white2 },
+          { name: 'x', areas: x },
+          { name: 'y', areas: y },
+          { name: 'Soul silver', areas: soulSilver },
+          { name: 'Heart Gold', areas: heartGold },
+          { name: 'Fire red', areas: fireRed },
+          { name: 'Leaf green', areas: leafGreen },
+        ];
+
+        // debugger;
+
+        setLocationAreaEncounters(allVersionData);
       })
       .catch((error) => {
         // handle error
@@ -37,19 +140,18 @@ const LocationArea = ({ selectedPokemon }) => {
           {locationAreaEncounters.length !== 0 && (
             <table className="location-area-table">
               <tr>
-                <th className="header">Area</th>
-                <th className="header">Version</th>
+                <th>Version</th>
+                <th>Area</th>
               </tr>
-              {locationAreaEncounters.map((area) => (
-                <tr className="area" key={area.location_area.name}>
-                  <td className="area-name">{area.location_area.name}</td>
-                  <td className="version">
-                    {area.version_details.map((version, index) => (
-                      <span className="version">
-                        {index !== area.version_details.length - 1
-                          ? `${version.version.name}, `
-                          : version.version.name}
-                      </span>
+              {locationAreaEncounters.map((game) => (
+                <tr key={game.name}>
+                  <td>{game.name}</td>
+                  <td>
+                    {game.areas.length === 0 ? 'None' : ''}
+                    {game.areas.map((area, index) => (
+                      <p className="area">
+                        {index !== game.areas.length - 1 ? `${area}, ` : area}
+                      </p>
                     ))}
                   </td>
                 </tr>
@@ -89,6 +191,11 @@ const StyledLocationArea = styled.div`
       padding: 0.25rem;
       text-align: center;
       padding: 1rem;
+    }
+
+    .area {
+      margin: 0.5rem;
+      display: inline-block;
     }
   }
 `;
