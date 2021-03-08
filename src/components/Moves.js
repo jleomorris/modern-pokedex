e7d2969eb286cbc1a1e503dc96dee1655456bf8d
jleomorris <70540466+jleomorris@users.noreply.github.com>
2021-03-08 02:48:37 +0000

    Add machine moves

--- src/components/Moves.js
+++ src/components/Moves.js
@@ -10,8 +10,11 @@ const Moves = ({ selectedPokemon }) => {
   // State
   const [ownMoves, setOwnMoves] = useState();
   const [canLearn, setCanLearn] = useState();
+  const [machineMoves, setmachineMoves] = useState();
   const [fullOwnMoveData, setFullOwnMoveData] = useState();
-  const [redBlueMoves, setRedBlueMoves] = useState(null);
+  const [fullMachineMoveData, setFullMachineMoveData] = useState();
+  const [redBlueLevelUpMoves, setRedBlueLevelUpMoves] = useState(null);
+  const [redBlueMachineMoves, setRedBlueMachineMoves] = useState(null);
 
   // Set up own moves and learnable moves
   useEffect(() => {
@@ -22,8 +25,13 @@ const Moves = ({ selectedPokemon }) => {
     const filteredLearnableMoves = allMoves.filter(
       (move) => move.version_group_details[0].level_learned_at === 0
     );
+    const filteredMachineMoves = allMoves.filter(
+      (move) =>
+        move.version_group_details[0].move_learn_method.name === 'machine'
+    );
 
     setOwnMoves(filteredOwnMoves);
+    setmachineMoves(filteredMachineMoves);
     setCanLearn(filteredLearnableMoves);
   }, [selectedPokemon]);
 
@@ -52,43 +60,146 @@ const Moves = ({ selectedPokemon }) => {
     }
   }, [ownMoves]);
 
-  // Set red and blue version move data
+  // Get full machine moves data
+  useEffect(async () => {
+    const allMachineMoveData = [];
+
+    if (machineMoves.length > 0) {
+      for (let i = 0; i < machineMoves.length; i += 1) {
+        const eachMachineMovesData = await axios.get(machineMoves[i].move.url);
+
+        allMachineMoveData.push(eachMachineMovesData.data);
+      }
+
+      await Promise.all(allMachineMoveData).then(() => {
+        console.table('allMachineMoveData', allMachineMoveData);
+
+        // merge with ownMoves to get version group data (level learned at)
+        const mergedData = allMachineMoveData.map((move, i) => ({
+          ...move,
+          ...machineMoves[i],
+        }));
+
+        const machineMovesSortedByName = mergedData.sort((a, b) =>
+          a.name > b.name ? 1 : -1
+        );
+
+        setFullMachineMoveData(machineMovesSortedByName);
+      });
+    }
+  }, [machineMoves]);
+
+  // Set red and blue version own move data
   useEffect(() => {
     if (fullOwnMoveData) {
-      const redBlueMovesFiltered = fullOwnMoveData.filter(
-        (move) =>
-          move.version_group_details[0].version_group.name === 'red-blue'
-      );
-
-      const filteredMovesUnique = [];
+      //   const redBlueMovesFiltered = fullOwnMoveData.filter(
+      //     (move) =>
+      //       move.version_group_details[0].version_group.name === 'red-blue'
+      //   );
+      const redBlueMovesFiltered = fullOwnMoveData.map((move) => move);
+      const filteredLevelUpMovesUnique = [];
 
       for (let i = 0; i < redBlueMovesFiltered.length; i += 1) {
         for (const ver of redBlueMovesFiltered[i].version_group_details) {
           if (
-            ver.version_group.name === 'red-blue' &&
+            // ver.version_group.name === 'red-blue' &&
             ver.move_learn_method.name === 'level-up'
           ) {
-            filteredMovesUnique.push(redBlueMovesFiltered[i]);
+            filteredLevelUpMovesUnique.push(redBlueMovesFiltered[i]);
             break;
           }
         }
       }
 
       // Sort moves by level ascending
-      const sortedByLevel = filteredMovesUnique.sort((a, b) =>
-        a.version_group_details[0].level_learned_at >
-        b.version_group_details[0].level_learned_at
-          ? 1
-          : -1
+      const levelUpMovesSortedByLevel = filteredLevelUpMovesUnique.sort(
+        (a, b) =>
+          a.version_group_details[0].level_learned_at >
+          b.version_group_details[0].level_learned_at
+            ? 1
+            : -1
       );
 
-      setRedBlueMoves(sortedByLevel);
+      setRedBlueLevelUpMoves(levelUpMovesSortedByLevel);
     }
   }, [fullOwnMoveData]);
+
+  // Set red and blue version machine move data
+  //   useEffect(() => {
+  //     if (fullOwnMoveData) {
+  //       const redBlueMovesFiltered = fullOwnMoveData.filter(
+  //         (move) =>
+  //           move.version_group_details[0].version_group.name === 'red-blue'
+  //       );
+
+  //       const filteredMachineMovesUnique = [];
+
+  //       for (let i = 0; i < redBlueMovesFiltered.length; i += 1) {
+  //         for (const ver of redBlueMovesFiltered[i].version_group_details) {
+  //           if (
+  //             ver.version_group.name === 'red-blue' &&
+  //             ver.move_learn_method.name === 'machine'
+  //           ) {
+  //             filteredMachineMovesUnique.push(redBlueMovesFiltered[i]);
+  //             break;
+  //           }
+  //         }
+  //       }
+
+  //       // Sort moves by level ascending
+  //       const machineMovesSortedByLevel = filteredMachineMovesUnique.sort(
+  //         (a, b) =>
+  //           a.version_group_details[0].level_learned_at >
+  //           b.version_group_details[0].level_learned_at
+  //             ? 1
+  //             : -1
+  //       );
+
+  //       setRedBlueMachineMoves(machineMovesSortedByLevel);
+  //     }
+  //   }, [fullMachineMoveData]);
 
   return (
     <StyledMoves className="move-container">
       <h3>Own moves</h3>
+      <table className="move-table">
+        <tr>
+          <th className="move-header">Name</th>
+          <th className="inner-heading">Level learned</th>
+          <th className="inner-heading">Accuracy</th>
+          <th className="inner-heading">Type</th>
+          <th className="inner-heading">Power</th>
+          <th className="inner-heading">PP</th>
+          <th className="inner-heading">Target</th>
+          <th className="inner-heading">Damage class</th>
+          <th className="inner-heading">Crit rate</th>
+          <th className="inner-heading">Version</th>
+        </tr>
+        {redBlueLevelUpMoves &&
+          redBlueLevelUpMoves.map((move) => (
+            <tr className="own-move" key={move.name}>
+              <td className="own-move-title">{move.name}</td>
+              <td className="level-learned">
+                {move.version_group_details[0].level_learned_at}
+              </td>
+              <td className="accuracy">
+                {move.accuracy ? move.accuracy : '-'}
+              </td>
+              <td className="type">{convertToTypeImage(move.type.name)}</td>
+              <td className="power">{move.power ? move.power : '-'}</td>
+              <td className="pp">{move.pp}</td>
+              <td className="target">{move.target.name}</td>
+              <td className="damage-class">
+                {convertDamageClassToImage(move.damage_class.name)}
+              </td>
+              <td className="power">{move.meta.crit_rate}</td>
+              <td className="version">
+                {move.version_group_details[0].version_group.name}
+              </td>
+            </tr>
+          ))}
+      </table>
+      <h3>Machine moves</h3>
       <table className="move-table">
         <tr>
           <th className="move-header">Name</th>
@@ -99,11 +210,10 @@ const Moves = ({ selectedPokemon }) => {
           <th className="inner-heading">Target</th>
           <th className="inner-heading">Damage class</th>
           <th className="inner-heading">Crit rate</th>
-          <th className="inner-heading">Level learned</th>
           <th className="inner-heading">Version</th>
         </tr>
-        {redBlueMoves &&
-          redBlueMoves.map((move) => (
+        {fullMachineMoveData &&
+          fullMachineMoveData.map((move) => (
             <tr className="own-move" key={move.name}>
               <td className="own-move-title">{move.name}</td>
               <td className="accuracy">
@@ -117,9 +227,6 @@ const Moves = ({ selectedPokemon }) => {
                 {convertDamageClassToImage(move.damage_class.name)}
               </td>
               <td className="power">{move.meta.crit_rate}</td>
-              <td className="level-learned">
-                {move.version_group_details[0].level_learned_at}
-              </td>
               <td className="version">
                 {move.version_group_details[0].version_group.name}
               </td>
